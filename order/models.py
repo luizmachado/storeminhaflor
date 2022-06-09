1217b485430a20cecde9da0d9f745a25444daa06
Luiz Augusto Machado <luizmachado.to@gmail.com>
2022-06-08 21:56:00 -0300

    Create Order model
    Create Order and OrderItem clases and install app order, register admin

--- order/models.py
+++ order/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Usuário')
+    total = models.FloatField(verbose_name='Total')
+    status = models.CharField(
+        verbose_name='Status',
+        default="C",
+        max_length=1,
+        choices=(
+            ('A', 'Aprovado'),
+            ('C', 'Criado'),
+            ('R', 'Reprovado'),
+            ('P', 'Pendente'),
+            ('E', 'Enviado'),
+            ('F', 'Finalizado'),
+        ),
+    )
+
+    def __str__(self) -> str:
+        return f'Pedido nº {self.pk}'
+
+    class Meta:
+        verbose_name = 'Pedido'
+        verbose_name_plural = 'Pedidos'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, verbose_name='Pedido')
+    product = models.CharField(max_length=255, verbose_name='Produto')
+    product_id = models.PositiveIntegerField(verbose_name='ID Produto')
+    variation = models.CharField(
+        max_length=255, verbose_name='Variação produto')
+    variation_id = models.PositiveIntegerField(
+        verbose_name='ID Variação produto')
+    price = models.FloatField(verbose_name='Preço')
+    # TODO: Verify the need of this property
+    price_discount = models.FloatField(
+        default=0, verbose_name='Valor promocional')
+    image = models.CharField(max_length=2000)
+
+
+def __str__(self):
+    return f'Item do {self.pedido}'
+
+
+class Meta:
+    verbose_name = 'Item do pedido'
+    verbose_name_plural = 'Itens do pedido'
