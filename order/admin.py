46e57b946810398e6a44fd572cda43ce56aea53a
Luiz Augusto Machado <luizmachado.to@gmail.com>
2022-06-09 23:44:27 -0300

    Add TabularInline into OrderAdmin
    Add TabularInline to create OrderItem easily inside Order form

--- order/admin.py
+++ order/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Order)
+class OrderItemInline(admin.TabularInline):
+    model = models.OrderItem
+    extra = 1
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderItemInline
+    ]
+
+admin.site.register(models.Order, OrderAdmin)
 admin.site.register(models.OrderItem)
 
