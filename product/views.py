f0a14b9242a2f166b5ae123462606d8283f78e2c
Luiz Augusto Machado <luizmachado.to@gmail.com>
2022-06-25 21:22:52 -0300

    Develop remove from cart function
    Enable icon in cart page to remove items from cart

--- product/views.py
+++ product/views.py
@@ -115,7 +115,32 @@ class AddCart(View):
 
 class RemoveFromCart(View):
     def get(self, *args, **kwargs):
-        return HttpResponse('RemoveFromCart')
+        http_referer = self.request.META.get(
+            'HTTP_REFERER',
+            reverse('product:list')
+        )
+        variation_id = self.request.GET.get('vid')
+
+        if not variation_id:
+            return redirect(http_referer)
+
+        if not self.request.session.get('cart'):
+            return redirect(http_referer)
+        
+        if variation_id not in self.request.session['cart']:
+            return redirect(http_referer)
+
+        cart = self.request.session['cart'][variation_id]
+
+        messages.success(
+            self.request,
+            f'Produto {cart["product_name"]} {cart["variation_name"]}'
+            f'removido do carrinho'
+        )
+        del self.request.session['cart'][variation_id]
+        self.request.session.save()
+
+        return redirect(http_referer)
 
 
 class Cart(View):
