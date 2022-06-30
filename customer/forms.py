eba7f33c57d74b02221cc977681d299df0c253ba
Luiz Augusto Machado <luizmachado.to@gmail.com>
2022-06-29 23:07:19 -0300

    Validate customer form
    Validate forms to create and update customer information, the
    function looks for existed user or email in the database in addition
    to check if password match with confirmation or if it lens greater
    then 6 chars

--- customer/forms.py
+++ customer/forms.py
@@ -1,3 +1,4 @@
+from urllib import request
 from django import forms
 from django.contrib.auth.models import User
 from . import models
@@ -36,3 +37,58 @@ class UserForm(forms.ModelForm):
         data = self.data
         cleaned = self.cleaned_data
 
+        validation_error_messages = {}
+
+        user_data = cleaned.get('username')
+        pass_data = cleaned.get('password')
+        pass2_data = cleaned.get('password2')
+        email_data = cleaned.get('email')
+
+        user_db = User.objects.filter(username=user_data).first()
+        email_db = User.objects.filter(email=email_data).first()
+
+        error_msg_user_exists = 'Usuário já existe'
+        error_msg_email_exists = 'E-mail já existe'
+        error_passwd_not_match = 'Senhas não conferem'
+        error_passwd_short = 'Senha precisa ter pelo menos 6 caracteres'
+        error_passwd_required_field = 'A senha é obrigatória'
+
+        # Update customer validation
+        if self.user:
+            if user_db:
+                if user_data != self.user.username:
+                    validation_error_messages['username'] = error_msg_user_exists
+
+            if email_db:
+                if email_data != self.user.email:
+                    validation_error_messages['email'] = error_msg_email_exists
+
+            if pass_data:
+                if pass_data != pass2_data:
+                    validation_error_messages['password'] = error_passwd_not_match
+                    validation_error_messages['password2'] = error_passwd_not_match
+                if len(pass_data) < 6:
+                    validation_error_messages['password'] = error_passwd_short
+
+        # Create User validation
+        else:
+            if user_db:
+                validation_error_messages['username'] = error_msg_user_exists
+
+            if email_db:
+                validation_error_messages['email'] = error_msg_email_exists
+
+            if not pass_data:
+                validation_error_messages['password'] = error_passwd_required_field
+            if not pass2_data:
+                validation_error_messages['password2'] = error_passwd_required_field
+            
+            if pass_data != pass2_data:
+                validation_error_messages['password'] = error_passwd_not_match
+                validation_error_messages['password2'] = error_passwd_not_match
+
+            if len(pass_data) < 6:
+                validation_error_messages['password'] = error_passwd_short
+        
+        if validation_error_messages:
+            raise(forms.ValidationError(validation_error_messages))
