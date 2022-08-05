6fa88d3ca136a265267265c8deccf4a011a4a888
Luiz Augusto Machado <luizmachado.to@gmail.com>
2022-08-04 23:31:09 -0300

    Deny access to order witch belongs to another user
    Make order inaccessible to anonymous or to another users, allow only
    for the user who create the order.

--- order/views.py
+++ order/views.py
@@ -13,6 +13,13 @@ from utils import accounting
 from . import models
 
 
+class DispatchLoginRequired(View):
+    def dispatch(self, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return redirect('customer:create')
+
+        return super().dispatch(*args, **kwargs)
+
 class SaveOrder(View):
     template_name = 'order/saveorder.html'
 
@@ -119,11 +126,16 @@ class SaveOrder(View):
         )
 
 
-class PayOrder(DetailView):
+class PayOrder(DispatchLoginRequired, DetailView):
     template_name = 'order/pay.html'
     model = models.Order
     pk_url_kwarg = 'pk'
     context_object_name = 'order'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(user=self.request.user)
+        return qs
 
 
 class DetailOrder(View):
